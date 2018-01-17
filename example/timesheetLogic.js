fafac43dc470300314295268c5e4c2291c02e0c6
jstudenski <jstudenski@gmail.com>
2018-01-17 07:45:18 -0600

    adding calculations

--- example/timesheetLogic.js
+++ example/timesheetLogic.js
@@ -1,14 +1,3 @@
-/* global firebase moment */
-// Steps to complete:
-
-// 1. Initialize Firebase
-// 2. Create button for adding new employees - then update the html + update the database
-// 3. Create a way to retrieve employees from the employee database.
-// 4. Create a way to calculate the months worked. Using difference between start and current time.
-//    Then use moment.js formatting to set difference in months.
-// 5. Calculate Total billed
-
-// 1. Initialize Firebase
 var config = {
   apiKey: "AIzaSyD0XPGwEVmiXGFHvjLsHsgUuN5_M80rucI",
   authDomain: "test-project-aaf9f.firebaseapp.com",
--- javascript/script.js
+++ javascript/script.js
@@ -1,40 +1,26 @@
 $('#first-train-input').mask('00:00');
 $('#frequency-input').mask('0#');
 
- // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCGR_Kf4yCwPvEX69GyfsBpAwPTOIdCx8M",
-    authDomain: "train-time-7cd31.firebaseapp.com",
-    databaseURL: "https://train-time-7cd31.firebaseio.com",
-    projectId: "train-time-7cd31",
-    storageBucket: "train-time-7cd31.appspot.com",
-    messagingSenderId: "1096060555195"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCGR_Kf4yCwPvEX69GyfsBpAwPTOIdCx8M",
+  authDomain: "train-time-7cd31.firebaseapp.com",
+  databaseURL: "https://train-time-7cd31.firebaseio.com",
+  projectId: "train-time-7cd31",
+  storageBucket: "train-time-7cd31.appspot.com",
+  messagingSenderId: "1096060555195"
+};
 
-
-// Create a variable to reference the database
+firebase.initializeApp(config);
 var database = firebase.database();
 
 
-
 // Current Time
-
-
-
 var updateTime = function () {
-    var currentTime = moment();
-    $("#current-time").html(moment(currentTime).format("H:mm"));
+  var currentTime = moment();
+  $("#current-time").html(moment(currentTime).format("H:mm:ss"));
 };
 setInterval(updateTime, 1000); // every second
-
-
-
-// $(document).ready(function(){
-//     datetime = $('#datetime')
-//     update();
-//     setInterval(update, 1000);
-// });
 
 
 
@@ -44,7 +30,7 @@ $("#submit").on("click", function() {
   database.ref().push({
     name: $("#name-input").val().trim(),
     destination: $("#destination-input").val().trim(),
-    startdate: $("#first-train-input").val().trim(),
+    starttime: $("#first-train-input").val().trim(),
     frequency: $("#frequency-input").val().trim()
   });
 
@@ -55,13 +41,45 @@ $("#submit").on("click", function() {
 
 });
 
-// console.log(moment().diff(moment([2014, 1, 1]), 'months', true));
-// console.log(moment().format("MM DD YY"));
 
 
 database.ref().on("child_added", function(snapshot, prevChildKey) {
 
-  console.log(snapshot.key);
+  console.log(snapshot.val());
+
+
+  // Assumptions
+  var tFrequency = 3;
+
+  // Time is 3:30 AM
+  var firstTime = "03:30";
+
+  // First Time (pushed back 1 year to make sure it comes before current time)
+  var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
+  console.log(firstTimeConverted);
+
+  // Current Time
+  var currentTime = moment();
+  console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+  // Difference between the times
+  var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+  console.log("DIFFERENCE IN TIME: " + diffTime);
+
+  // Time apart (remainder)
+  var tRemainder = diffTime % tFrequency;
+  console.log(tRemainder);
+
+  // Minute Until Train
+  var tMinutesTillTrain = tFrequency - tRemainder;
+  console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+  // Next Train
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+  console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+
+  // generate remove button
   var btn = $("<button>");
     btn.addClass("item-btn");
     btn.attr("data-key", snapshot.key);
@@ -71,10 +89,11 @@ database.ref().on("child_added", function(snapshot, prevChildKey) {
     btn.append(i);
     btn.click(remove);
 
+  // generate table
   var $tr = $('<tr>').append(
     $('<td>').text(snapshot.val().name),
     $('<td>').text(snapshot.val().destination),
-    $('<td>').text(snapshot.val().startdate),
+    $('<td>').text(snapshot.val().starttime),
     $('<td>').text(snapshot.val().frequency),
     $('<td>').text('TEXT2'),
     $('<td>').append(btn)
